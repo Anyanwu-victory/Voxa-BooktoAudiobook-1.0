c393256846fef22886554511348910e03ac35550
Anyanwu-victory <118054739+Anyanwu-victory@users.noreply.github.com>
2025-11-20 19:53:51 +0100

    Building an open text to speech tool with no authentication, Just You , your books and audios

---
+++ frontend/app/_data/features.ts
@@ -0,0 +1,24 @@
+import { EyeIcon, GlobeIcon, ServerCogIcon, ZapIcon } from "lucide-react";
+
+export const features = [
+    {
+      name: "Listen to Books Anytime",
+      description: "Upload a book or text, and the website will turn it into an audiobook so you can listen instead of reading.",
+      icon: GlobeIcon
+    },
+    {
+      name: "Support Multiple Languages",
+      description: "The platform can read and summarize books in English, French, and Spanish.",
+      icon: ZapIcon
+    },
+    {
+      name: "Quick Summaries",
+      description: "Get a short, easy-to-read summary of any book in different languages.",
+      icon: EyeIcon
+    },
+    {
+      name: "Cloud Backup",
+      description: "Automatically back up your documents to the cloud for peace of mind.",
+      icon: ServerCogIcon
+    }
+  ]
